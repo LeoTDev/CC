fe29a4c4fdd6ef060d05909620d90dfe09dca3fc
Leonardo Tome <leotometerra@gmail.com>
2016-05-23 16:57:57 -0300

    initial commit

---
+++ FreshlyPressed/src/main/java/com/test/cc/PostsActivity.java
@@ -0,0 +1,17 @@
+package com.test.cc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostsActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.posts_activity);
+
+    }
+
+}
